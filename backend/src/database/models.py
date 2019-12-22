e10c6510d1e96f82b60a026b38365c9b66ecef7e
Noreen Wu <noreenwu@gmail.com>
2019-12-22 12:33:44 -0500

    clean: remove unused code

--- backend/src/database/models.py
+++ backend/src/database/models.py
@@ -106,9 +106,9 @@ class Drink(db.Model):
     def __repr__(self):
         return json.dumps(self.short())
 
-    def format(self):
-        return {
-            'id': self.id,
-            'title': self.title,
-            'recipe': self.recipe
-        }
+    # def format(self):
+    #     return {
+    #         'id': self.id,
+    #         'title': self.title,
+    #         'recipe': self.recipe,
+    #     }
