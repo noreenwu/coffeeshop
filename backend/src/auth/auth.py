07510d7c863e0819d8cdacb53ea1c52633bbc74e
Noreen Wu <noreenwu@gmail.com>
2019-12-22 12:33:00 -0500

    clean: mostly PEP8-compliant

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -11,19 +11,17 @@ ALGORITHMS = ['RS256']
 API_AUDIENCE = 'drink'
 
 
-## AuthError Exception
-'''
-AuthError Exception
-A standardized way to communicate auth failure modes
-'''
+# -------------------------------------------------------------
+# AuthError Exception
+#   A standardized way to communicate auth failure modes
+# -------------------------------------------------------------
 class AuthError(Exception):
     def __init__(self, error, status_code):
         self.error = error
         self.status_code = status_code
 
 
-## Auth Header
-
+# Auth Header
 '''
 @TODO implement get_token_auth_header() method
     it should attempt to get the header from the request
@@ -37,43 +35,25 @@ def get_token_auth_header():
     """
     auth = request.headers.get('Authorization', None)
     if not auth:
-        print ("authorization header missing")
+        print("authorization header missing")
         abort(401)
-        # raise AuthError({
-        #     'code': 'authorization_header_missing',
-        #     'description': 'Authorization header is expected.'
-        # }, 401)
 
     parts = auth.split()
     if parts[0].lower() != 'bearer':
-        print ("authorization header must start with Bearer")
+        print("authorization header must start with Bearer")
         abort(401)
-        # raise AuthError({
-        #     'code': 'invalid_header',
-        #     'description': 'Authorization header must start with "Bearer".'
-        # }, 401)
 
     elif len(parts) == 1:
-        print ("Token not found")
+        print("Token not found")
         abort(401)
-        # raise AuthError({
-        #     'code': 'invalid_header',
-        #     'description': 'Token not found.'
-        # }, 401)
 
     elif len(parts) > 2:
-        print ("Invalid header: authorization header must be bearer token")
+        print("Invalid header: authorization header must be bearer token")
         abort(401)
-        # raise AuthError({
-        #     'code': 'invalid_header',
-        #     'description': 'Authorization header must be bearer token.'
-        # }, 401)
 
     token = parts[1]
     return token
 
-# def get_token_auth_header():
-#    raise Exception('Not Implemented')
 
 '''
 @TODO implement check_permissions(permission, payload) method
@@ -83,7 +63,8 @@ def get_token_auth_header():
 
     it should raise an AuthError if permissions are not included in the payload
         !!NOTE check your RBAC settings in Auth0
-    it should raise an AuthError if the requested permission string is not in the payload permissions array
+    it should raise an AuthError if the requested permission string is not in the
+    payload permissions array
     return true otherwise
 '''
 # def check_permissions(permission, payload):
@@ -93,17 +74,10 @@ def get_token_auth_header():
 def check_permissions(permission, payload):
     if 'permissions' not in payload:
         abort(400)
-                        # raise AuthError({
-                        #     'code': 'invalid_claims',
-                        #     'description': 'Permissions not included in JWT.'
-                        # }, 400)
 
     if permission not in payload['permissions']:
         abort(403)
-        # raise AuthError({
-        #     'code': 'unauthorized',
-        #     'description': 'Permission not found.'
-        # }, 403)
+
     return True
 
 
@@ -117,11 +91,9 @@ def check_permissions(permission, payload):
     it should decode the payload from the token
     it should validate the claims
     return the decoded payload
-
-    !!NOTE urlopen has a common certificate error described here: https://stackoverflow.com/questions/50236117/scraping-ssl-certificate-verify-failed-error-for-http-en-wikipedia-org
 '''
 def verify_decode_jwt(token):
-    print ("verify_decode_jwt token is ", token)
+    # print ("verify_decode_jwt token is ", token)
     jsonurl = urlopen(f'https://{AUTH0_DOMAIN}/.well-known/jwks.json')
     jwks = json.loads(jsonurl.read())
     unverified_header = jwt.get_unverified_header(token)
@@ -129,7 +101,6 @@ def verify_decode_jwt(token):
     if 'kid' not in unverified_header:
         print("invalid header")
         abort(401)
-
 
     for key in jwks['keys']:
         if key['kid'] == unverified_header['kid']:
@@ -150,8 +121,7 @@ def verify_decode_jwt(token):
                 audience=API_AUDIENCE,
                 issuer='https://' + AUTH0_DOMAIN + '/'
             )
-
-            print ("rsa_key is ", rsa_key)
+            # print ("rsa_key is ", rsa_key)
             return payload
 
         except jwt.ExpiredSignatureError:
@@ -175,8 +145,10 @@ def verify_decode_jwt(token):
 
     it should use the get_token_auth_header method to get the token
     it should use the verify_decode_jwt method to decode the jwt
-    it should use the check_permissions method validate claims and check the requested permission
-    return the decorator which passes the decoded payload to the decorated method
+    it should use the check_permissions method validate claims and
+    check the requested permission
+    return the decorator which passes the decoded payload to the
+    decorated method
 '''
 def requires_auth(permission=''):
     def requires_auth_decorator(f):
